e686388939dcdedfac2c0e81d6cbc09301eee254
Gursimrat Singh Kalra <gursimratsingh.kalra@inovaare.com>
2024-04-27 11:34:54 +0530

    feat(backend): modified schema and controllers status(working fine)

--- backend/controllers/attendanceController.js
+++ backend/controllers/attendanceController.js
@@ -21,9 +21,21 @@ const createAttendance = asyncHandler(async (req, res) => {
 // route  get /api/attendance
 // @access Private
 const getCurrentUserAttendance = asyncHandler(async (req, res) => {
+  const { month, year } = req.query
+  if (!month || !year) {
+    return res.status(400).send("Invalid Route")
+  }
+
+  const startDate = `${year}-${month}-01`
+  const endDate = `${year}-${month < 12 ? month + 1 : 1}-01`
+
   const attendance = await prisma.attendance.findMany({
     where: {
-      userId: req.user.id,
+      AND: [
+        { userId: req.user.id },
+        { date: { gte: startDate } },
+        { date: { lt: endDate } }
+      ]
     },
   });
 
