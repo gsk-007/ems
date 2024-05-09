d13435aeb5bcd6b6576af9199b860bb077eb1233
Gursimrat Singh Kalra <gursimratsingh.kalra@inovaare.com>
2024-05-09 10:38:26 +0530

    prod update

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res, next) => {
-  return res.send("API is Running");
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/department", departmentRoutes);
@@ -38,10 +34,10 @@ app.use("/api/upload", uploadRoutes);
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // empty string because we are using a proxy
-export const baseQuery = fetchBaseQuery({ baserUrl: "" })
+export const baseQuery = fetchBaseQuery({
+  baserUrl: "http://localhost:5000/api/",
+});
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
   endpoints: (builder) => ({}),
-})
+});
